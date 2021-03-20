feb409d3c14d218f0760672eedb200c93a0473ca
Tanmay Singal <tanmaysingal@icloud.com>
2021-03-20 17:14:31 +0530

    Like and unlike posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const {AuthenticationError} = require('apollo-server');
+const {UserInputError, AuthenticationError} = require('apollo-server');
 
 const Post = require('../../models/Post')
 const checkAuth = require('../../util/checkAuth')
@@ -50,6 +50,30 @@ module.exports = {
                     return "Post deleted successfully";
                 } else {
                     throw new AuthenticationError("User not authorized to perform this action");
+                }
+            } catch (e){
+                throw new Error(e);
+            }
+        },
+        async likePost(_, {postId}, context){
+            const {username } = checkAuth(context);
+
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    // Checks if post already liked
+                    if(post.likes.find(like => like.username === username)){
+                        post.likes = post.likes.filter(like => like.username !== username);
+                    } else {
+                        post.likes.push({
+                            username,
+                            createdAt: new Date().toISOString()
+                        })
+                    }
+                    await post.save();
+                    return post;
+                } else {
+                    throw new UserInputError("Post not found");
                 }
             } catch (e){
                 throw new Error(e);
