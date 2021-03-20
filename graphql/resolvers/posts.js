b3ad43845c6d81f3d0c740d6c08634ec903df67d
Tanmay Singal <tanmaysingal@icloud.com>
2021-03-20 15:09:28 +0530

    Deleting posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const {AuthenticationError} = require('apollo-server');
+
 const Post = require('../../models/Post')
 const checkAuth = require('../../util/checkAuth')
 
@@ -26,7 +28,7 @@ module.exports = {
         }
     },
     Mutation: {
-        async createPost(_, {body}, context ){
+        async createPost(_, {body}, context){
             const user = checkAuth(context);
             const newPost = new Post({
                 body,
@@ -37,6 +39,21 @@ module.exports = {
 
             const post = await newPost.save();
             return post;
+        },
+        async deletePost(_, {postId}, context){
+            const user = checkAuth(context);
+
+            try {
+                const post = await Post.findById(postId);
+                if (post.username === user.username) {
+                    await post.delete();
+                    return "Post deleted successfully";
+                } else {
+                    throw new AuthenticationError("User not authorized to perform this action");
+                }
+            } catch (e){
+                throw new Error(e);
+            }
         }
     }
 }
