642ad32b3921e2fecd2c93bb14d33fe8e2799787
Tanmay Singal <tanmaysingal@icloud.com>
2021-03-21 22:28:58 +0530

    Tried to setup register

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,31 +4,6 @@ import gql from 'graphql-tag'
 import {Grid} from "semantic-ui-react";
 import PostCard from "../components/PostCard";
 
-function Home() {
-    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS_QUERY)
-
-    return (
-        <div>
-            <Grid columns={3} divided>
-                <Grid.Row>
-                    <h1>Recent Posts</h1>
-                </Grid.Row>
-                <Grid.Row>
-                    {loading ? (
-                        <h1>Loading Posts...</h1>
-                    ) : (
-                        posts && posts.map(post => (
-                            <Grid.Column key={post.id} style={{marginBottom : 20}}>
-                                <PostCard post={post}/>
-                            </Grid.Column>
-                        ))
-                    )}
-                </Grid.Row>
-            </Grid>
-        </div>
-    );
-}
-
 const FETCH_POSTS_QUERY = gql`
     {
         getPosts{
@@ -38,7 +13,7 @@ const FETCH_POSTS_QUERY = gql`
             username
             likeCount
             likes{
-               username
+                username
             }
             commentCount
             comments{
@@ -50,4 +25,28 @@ const FETCH_POSTS_QUERY = gql`
         }
     }
 `
+
+function Home() {
+
+    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS_QUERY)
+    return (
+        <Grid columns={3} divided>
+            <Grid.Row className="page-title">
+                <h1>Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading Posts...</h1>
+                ) : (
+                    posts && posts.map(post => (
+                        <Grid.Column key={post.id} style={{marginBottom: 20}}>
+                            <PostCard post={post}/>
+                        </Grid.Column>
+                    ))
+                )}
+            </Grid.Row>
+        </Grid>
+    );
+
+}
 export default Home;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,99 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import {Form, Button} from 'semantic-ui-react'
+import {useMutation} from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
-class Register extends Component {
-    render() {
-        return (
-            <div>
+function Register() {
 
-            </div>
-        );
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword:''
+    })
+
+    const onChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value})
     }
+
+    const [addUser, {loading}] = useMutation(REGISTER_USER, {
+        update(proxy, result){
+            console.log(result)
+        },
+        variable: values
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addUser();
+    }
+
+    return (
+        <div className="form-container">
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label='Username'
+                    placeholder='Username'
+                    name='username'
+                    type='text'
+                    value={values.username}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label='Email'
+                    placeholder='Email'
+                    name='email'
+                    type='email'
+                    value={values.email}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label='Password'
+                    placeholder='Password'
+                    name='password'
+                    type='password'
+                    value={values.password}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label='Confirm Password'
+                    placeholder='Confirm Password'
+                    name='confirmPassword'
+                    type='password'
+                    value={values.confirmPasssword}
+                    onChange={onChange}
+                />
+                <Button type="submit" primary>
+                    Register
+                </Button>
+            </Form>
+        </div>
+    );
 }
+
+const REGISTER_USER = gql`
+    mutation register(
+        $username: String!
+        $email: String!
+        $password: String!
+        $confirmPassword: String!
+    ) {
+        register(
+            registerInput: {
+                username: $username
+                email: $email
+                password: $password
+                confirmPassword: $confirmPassword
+            }
+        ) {
+            id
+            email
+            username
+            createdAt
+            token
+        }
+    }
+`;
 
 export default Register;
